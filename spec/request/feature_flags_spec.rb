8f72a982b8acddd9d41f7e75ac7461a20cc0c78c
Piyali Banerjee <pbanerjee@pivotal.io>
2019-02-14 16:35:02 -0800

    API user can see the details of a feature flag
    [Finishes #162748045]
    
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- spec/request/feature_flags_spec.rb
+++ spec/request/feature_flags_spec.rb
@@ -34,4 +34,50 @@ RSpec.describe 'Feature Flags Request' do
       end
     end
   end
+
+  describe 'GET /v3/feature_flags/:name' do
+    let(:user) { make_user }
+    let(:headers) { headers_for(user) }
+
+    context 'there is not an override' do
+      it 'returns details of the requested feature flag when ' do
+        get '/v3/feature_flags/diego_docker', nil, headers
+        expect(last_response.status).to eq 200
+        expect(parsed_response).to be_a_response_like(
+          {
+            'updated_at' => nil,
+            'name' => 'diego_docker',
+            'enabled' => false,
+            'custom_error_message' => nil,
+            'links' => {
+              'self' => {
+                'href' => "#{link_prefix}/v3/feature_flags/diego_docker"
+              }
+            }
+          }
+        )
+      end
+    end
+
+    context 'there is an override' do
+      let(:feature_flag) { VCAP::CloudController::FeatureFlag.make(name: 'diego_docker', enabled: true, error_message: 'error') }
+      it 'returns details of the requested feature flag when there is an override' do
+        get "/v3/feature_flags/#{feature_flag.name}", nil, headers
+        expect(last_response.status).to eq 200
+        expect(parsed_response).to be_a_response_like(
+          {
+            'updated_at' => iso8601,
+            'name' => feature_flag.name,
+            'enabled' => feature_flag.enabled,
+            'custom_error_message' => feature_flag.error_message,
+            'links' => {
+              'self' => {
+                'href' => "#{link_prefix}/v3/feature_flags/diego_docker"
+              }
+            }
+          }
+        )
+      end
+    end
+  end
 end
