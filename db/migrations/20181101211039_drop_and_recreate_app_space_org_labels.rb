6d16edaef17422f51e0797526e6333fbd1fc8778
Andy Brown <anbrown@pivotal.io>
2019-03-18 17:38:15 -0700

    Make recent migrations reversible
    Don't run the first-cut-label removing actions in a transaction.
    See the associated story for the reason why
    
    [#164182536]

--- db/migrations/20181101211039_drop_and_recreate_app_space_org_labels.rb
+++ db/migrations/20181101211039_drop_and_recreate_app_space_org_labels.rb
@@ -1,19 +1,20 @@
 Sequel.migration do
+  no_transaction
   up do
     # These tables were never present in a released capi-release so it is safe to drop them
-    drop_table(:app_labels)
+    run 'drop table if exists app_labels;'
     create_table(:app_labels) do
       VCAP::Migration.common(self)
       VCAP::Migration.labels_common(self, :app_labels, :apps)
     end
 
-    drop_table(:org_labels)
+    run 'drop table if exists organization_labels;'
     create_table(:organization_labels) do
       VCAP::Migration.common(self)
       VCAP::Migration.labels_common(self, :organization_labels, :organizations)
     end
 
-    drop_table(:space_labels)
+    run 'drop table if exists space_labels;'
     create_table(:space_labels) do
       VCAP::Migration.common(self)
       VCAP::Migration.labels_common(self, :space_labels, :spaces)
@@ -21,6 +22,46 @@ Sequel.migration do
   end
 
   down do
-    # This migration cannot be reversed
+    run 'drop table if exists organization_labels;'
+    create_table(:organization_labels) do
+      VCAP::Migration.common(self)
+
+      String :org_guid, size: 255
+      String :key_prefix, size: 253
+      String :key_name, size: 63
+      String :value, size: 63
+
+      foreign_key [:org_guid], :organizations, key: :guid, name: :fk_organization_labels_org_guid
+      index [:org_guid], name: :fk_organization_labels_org_guid_index
+      index [:key_prefix, :key_name, :value], name: :organization_labels_compound_index
+    end
+
+    run 'drop table if exists space_labels;'
+    create_table(:space_labels) do
+      VCAP::Migration.common(self)
+
+      String :space_guid, size: 255
+      String :key_prefix, size: 253
+      String :key_name, size: 63
+      String :value, size: 63
+
+      foreign_key [:space_guid], :spaces, key: :guid, name: :fk_space_labels_space_guid
+      index [:space_guid], name: :fk_space_labels_space_guid_index
+      index [:key_prefix, :key_name, :value], name: :space_labels_compound_index
+    end
+
+    run 'drop table if exists app_labels;'
+    create_table(:app_labels) do
+      VCAP::Migration.common(self)
+
+      String :app_guid, size: 255
+      String :key_prefix, size: 253
+      String :key_name, size: 63
+      String :value, size: 63
+
+      foreign_key [:app_guid], :apps, key: :guid, name: :fk_app_labels_app_guid
+      index [:app_guid], name: :fk_app_labels_app_guid_index
+      index [:key_prefix, :key_name, :value], name: :app_labels_compound_index
+    end
   end
 end
--- db/migrations/20190222004908_remove_revisions_encrypted_commands_by_process_type.rb
+++ db/migrations/20190222004908_remove_revisions_encrypted_commands_by_process_type.rb
@@ -1,7 +1,12 @@
 Sequel.migration do
-  change do
+  up do
     alter_table(:revisions) do
       drop_column :encrypted_commands_by_process_type
+    end
+  end
+  down do
+    alter_table(:revisions) do
+      add_column :encrypted_commands_by_process_type, String, size: 4096
     end
   end
 end
